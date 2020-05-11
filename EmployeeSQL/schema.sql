590729cece89de85c281bfcfaf819c0fe0c7d93c
sjj1971 <sjj1971@gmail.com>
2020-05-10 21:56:02 -0700

    Added ERD figure and update schema according to ERD

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -8,47 +8,47 @@ drop table departments;
 -- define tables
 create table departments(
 	dept_no varchar(30) not null,
-	dept_name varchar(30),
+	dept_name varchar(30) not null,
 	primary key (dept_no)
 );
 
 create table titles(
-	title_id varchar(30),
-	title varchar(30),
+	title_id varchar(30) not null,
+	title varchar(30) not null,
 	primary key (title_id)
 );
 
 create table employees(
-	emp_no integer,
-	emp_title_id varchar(30),
+	emp_no integer not null,
+	emp_title_id varchar(30) not null,
 	foreign key (emp_title_id) references titles(title_id),
 	birth_date varchar(30),
-	first_name varchar(30),
-	last_name varchar(30),
+	first_name varchar(30) not null,
+	last_name varchar(30) not null,
 	sex char(10),
-	hire_date varchar(30),
+	hire_date varchar(30) not null,
 	primary key (emp_no)
 );
 
 create table dept_emp(
-	emp_no integer,
+	emp_no integer not null,
 	foreign key (emp_no) references employees(emp_no),
-	dept_no varchar(30),
+	dept_no varchar(30) not null,
 	foreign key (dept_no) references departments(dept_no),
 	primary key (emp_no, dept_no)
 );
 
 create table dept_manager(
-	dept_no varchar(30),
+	dept_no varchar(30) not null,
 	foreign key (dept_no) references departments(dept_no),
-	emp_no integer,
+	emp_no integer not null,
 	foreign key (emp_no) references employees(emp_no)
 );
 
 create table salaries(
-	emp_no integer,
+	emp_no integer not null,
 	foreign key (emp_no) references employees(emp_no),
-	salary integer
+	salary integer not null
 );
 
 -- after importing csv files for tables
